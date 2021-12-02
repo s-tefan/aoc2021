6e507e85dac020e7174f6d244b1224dd2ade91cb
s-tefan <github@k-son.se>
2021-12-02 20:42:47 +0100

    oneliner

---
+++ 01/aoc2021_01_var2.py
@@ -0,0 +1,8 @@
+print(
+    (lambda depths :  
+        (
+            [(depths[n+1]-depths[n]) > 0 for n in range(len(depths)-1)].count(True),
+            [(depths[n+3]-depths[n]) > 0 for n in range(len(depths)-3)].count(True)
+            )
+        )([int(x) for x in open("input.txt")])
+    )
