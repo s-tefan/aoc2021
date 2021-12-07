292c9382194ab59ba13c05b39c2351fbf7441b13
s-tefan <github@k-son.se>
2021-12-07 19:44:33 +0100

    Day 7, more finishing

--- 07/aoc2021_07.py
+++ 07/aoc2021_07.py
@@ -16,23 +16,20 @@ def sgn(x):
     return int(x>0) - int(x<0)
 
 def optimize2(crabpos):
-    a = sum(crabpos)/len(crabpos)
-    a += sum(sgn(x-a) for x in crabpos)/2/len(crabpos)
-    print(a)
+    a = sum(crabpos)/len(crabpos) # arithmetic mean optimizes squared distance sum, approx. for this
+    a += sum(sgn(x-a) for x in crabpos)/2/len(crabpos) # adjust a little, hopefully getting better approx
+    print(a, optfun(crabpos, a))
     b, c = (optfun(crabpos, int(a)), optfun(crabpos, int(a+1)))
-    print(optfun(crabpos,a))
     return (int(a),b) if b <= c else (int(a)+sgn(a),c)
-    # hopefully this gives the true optimum, should really check by stepping ...
+    # hopefully this gives the true optimal integer, should really check by stepping ...
 
 test = [16,1,2,0,4,2,7,1,2,14]
 print("Test set ({})".format(len(test)))
 print(optimize1sort(test))
 print(optimize2(test))
-print('Mean pos', sum(test)/len(test))
 with open("input.txt") as f:
     input = [int(x) for x in f.readline().split(',')]
 print("Input set ({})".format(len(input)))
 print(optimize1sort(input))
 print(optimize2(input))
-print('Mean pos', sum(input)/len(input))
 
