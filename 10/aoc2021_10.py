f05e2c03a86adb512f0a11de55ec625c9198dc2a
s-tefan <github@k-son.se>
2021-12-10 14:20:53 +0100

    Day 10 finished

--- 10/aoc2021_10.py
+++ 10/aoc2021_10.py
@@ -1,40 +1,33 @@
 
-def partone(input):
-    scorelist = []
+def partonetwo(input):
+    scorelist, score2list = [], []
     for line in input:
-        linescore = parse(iter(line))
+        linescore, openings = parse([], iter(line))
         scorelist.append(linescore)
-        print()
-    return scorelist
+        if not linescore:
+            s = 0
+            while openings:
+                s *= 5
+                s += score2[openings.pop()]
+            score2list.append(s)
+    return sum(scorelist), sorted(score2list)[len(score2list)//2]
 
 pairs = {'<': '>', '[': ']', '(': ')', '{': '}'}
 score = {')': 3, ']': 57, '}': 1197, '>': 25137}
-
-# Det blr inte rätt
-def getchunk(iterline, c):
+score2 = {'(': 1, '[': 2, '{': 3, '<': 4}
+def parse(cs, iterline):
     n = next(iterline)
     if n in pairs:
-        print(c, end='')
-        return getchunk(iterline, c) # Här får kollas
+        cs.append(n)
+        return parse(cs, iterline)
     elif n in pairs.values():
-        if n == pairs[c]:
-            print(n, end='')
-            return 0
+        if pairs[cs[-1]] == n:
+            return parse(cs[:-1], iterline)
         else:
-            print(n, end='!')
-            return score[n]
+            return score[n], cs
     else:
-        return 0
+        return 0, cs
 
-
-
-def parse(iterline):
-    c = next(iterline)
-    while True:
-        getchunk(iterline, c)
-
-
-
-with open("test.txt") as f:
+with open("input.txt") as f:
     input = f.readlines()
-    print(partone(input))
+    print(partonetwo(input))
