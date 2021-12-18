86ad03aaed24abce6efb26a75dd85e792c9255af
Stefan K <40999112+s-tefan@users.noreply.github.com>
2021-12-18 21:38:02 +0100

    Day 16 and 17 completed

--- 16/aoc2021_16.py
+++ 16/aoc2021_16.py
@@ -1,47 +1,98 @@
 
 class BitString:
+    version_sum = 0
+
     def __init__(self, s):
         self.string = s
 
     def pop(self, n):
-        x = int(self.string[:3], 2)
-        self. string = self.string[3:]
+        if n > len(self.string):
+            raise Exception("Short end: {}. Version sum: {}".format(
+                 self.string, BitString.version_sum))
+        x = int(self.string[:n], 2)
+        self.string = self.string[n:]
         return x
 
-    def pushbit(self, b):
-        if b:
-            self.string = "1" + self.string
-        else:
-            self.string = "0" + self.string
+    def str_pop(self, n):
+        if n > len(self.string):
+            raise Exception("Short end: {}".format(self.string))
+        x = self.string[:n]
+        self.string = self.string[n:]
+        return x
 
-    def readpacket(self):
-        version = self.pop(3)
-        id = self.pop(3)
-        match id:
-            case 4:
-                apa = 0
+    def readpacket(self, length = None):
+        if not self.string:
+            return 0
+        n = 0
+        try:
+            version = self.pop(3)
+            BitString.version_sum += version
+            id = self.pop(3)
+            n += 6
+            if id == 4:
+                value = 0
                 while self.pop(1) == 1:
-                    apa +<< 4
-                    apa += self.pop(4)
-                apa +<< 4
-                apa += self.pop(4)
-                while next(dataiter == 0):
-                    pass
-                self.pushbit(1)
-            case _:
+                    value <<= 4
+                    value += self.pop(4)
+                    n += 4
+                value <<= 4
+                value += self.pop(4)
+                n += 4
+            else:
                 mode = self.pop(1)
                 match mode:
                     case 0:
                         length = self.pop(15)
+                        sub_str = self.str_pop(length)
+                        bs = BitString(sub_str)
+                        ret_list = []
+                        while bs.string:
+                            ret_list.append(bs.readpacket())
                     case 1:
                         number = self.pop(11)
+                        ret_list = []
+                        for k in range(number):
+                            ret_list.append(self.readpacket())
+                if id == 0:
+                    value = sum(ret_list)
+                elif id == 1:
+                    value = 1
+                    for x in ret_list:
+                        value *= x
+                elif id == 2:
+                    value = min(ret_list)
+                elif id == 3:
+                    value = max(ret_list)
+                elif id == 5:
+                    value = int(ret_list[0] > ret_list[1])
+                elif id == 6:
+                    value = int(ret_list[0] < ret_list[1])
+                elif id == 7:
+                    value = int(ret_list[0] == ret_list[1])
+            return_value = {
+                "version": version,
+                "id": id,
+                "value": value}
+            return return_value["value"]
+
+        except Exception:
+            print("Kuken!")
+            raise Exception("Nu är det slut")
+            return 0
+
 
 
 def main():
     with open("input.txt") as f:
         hexdata = f.readline().strip()
         f.close()
-    bs = BitString(hex_to_binary(hexdata)))
+    bs = BitString(hex_to_binary(hexdata))
+    while len(bs.string) > 7:
+        try:
+            result = bs.readpacket()
+        except:
+            break
+    print("Part 1. Version sum: {}\nPart 2. Expression result: {}".format(BitString.version_sum, result))
 
 
 
--- 17/aoc2021_17.py
+++ 17/aoc2021_17.py
@@ -4,9 +4,37 @@ def main():
         line = f.readline().strip().split()
         xx = tuple(int(x) for x in line[2].split(',')[0].split('=')[1].split('..'))
         yy = tuple(int(x) for x in line[3].split('=')[1].split('..'))
-        print(partone(yy)
+        print(partone(yy))
+        print(parttwo(xx,yy))
 
 def partone(yy):
     return(-yy[0]*(-yy[0]-1)//2)
+
+def parttwo(xx,yy):
+    xrange = range(xx[0],xx[1]+1)
+    yrange = range(yy[0],yy[1]+1)
+    max_vx = xx[1]
+    min_vy = yy[0]
+    max_vy = -yy[0]
+    vset = set()
+    plist = list()
+    for vx in range(1, max_vx + 1):
+        for vy in range(min_vy , max_vy +1):
+            k = 1
+            #for k in range(1, max_vx):
+            continuing = True
+            while continuing:
+                x = k*vx - k*(k-1)//2 if k <= vx else vx*(vx+1)//2
+                y = k*vy - k*(k-1)//2
+                if x > max(xrange): break
+                if y < min(yrange): break
+                if x in xrange and y in yrange:
+                    print(vx, vy, x, y)
+                    vset.add((vx,vy))
+                    plist.append((x,y))
+                k += 1
+
+
+    return(len(vset))
 
 main()
