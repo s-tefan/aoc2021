dde6fce533388e0f0d8b51521ad7b23488d6bbb6
S-tefan <stefan@k-son.se>
2021-12-20 17:12:46 +0100

    Day 20 completed

--- 20/aoc2021_20.py
+++ 20/aoc2021_20.py
@@ -7,39 +7,59 @@ def main(filename):
         while True:
             line = f.readline().strip()
             if line:
-                print(line)
                 for j, c in enumerate(line):
                     if c == '#':
                         light_set.add((i,j))
                 i += 1
             else:
                 break
-    print("Algorithm:", algorithm)
-    print("Light set:", light_set)
     print(partone(light_set, algorithm))
+    print(parttwo(light_set, algorithm))
 
 def neighbourlist(p):
     neighbour_dist_list = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)]
     return [(p[0]+n[0], p[1]+n[1]) for n in neighbour_dist_list]
 
-def scan(light_set, algorithm):
-    rownumbers = [p[0] for p in light_set]
-    colnumbers = [p[1] for p in light_set]
-    rowrange = range(min(rownumbers) - 1, max(rownumbers) + 2)
-    colrange = range(min(colnumbers) - 1, max(colnumbers) + 2)
+def scan(light_set, algorithm, rowrange, colrange):
     new_light_set = set()
-    for light in ((i,j) for i in rowrange for j in colrange):
-        if algorithm[int(''.join([('1' if n in light_set else '0')
-            for n in neighbourlist(light)]), 2)] == '#':
-                new_light_set.add(light)
+    for pos in ((i,j) for i in rowrange for j in colrange):
+        if algorithm[int(''.join(['1' if n in light_set else '0'
+            for n in neighbourlist(pos)]), 2)] == '#':
+                new_light_set.add(pos)
     return new_light_set
 
+def print_image(lightset, a, b, c, d):
+        for r in range(a, b+1):
+            row = ""
+            for k in range(c, d+1):
+                row += "#" if (r,k) in lightset else "."
+            print(row)
+
 def partone(lights0, algorithm):
-    #print(lights0)
-    lights1 = scan(lights0, algorithm)
-    #print(lights1)
-    lights2 = scan(lights1, algorithm)
-    #print(lights2)
-    return len(lights2)
+    rownumbers = [p[0] for p in lights0]
+    colnumbers = [p[1] for p in lights0]
+    rowrange = range(min(rownumbers) - 3, max(rownumbers) + 4)
+    colrange = range(min(colnumbers) - 3, max(colnumbers) + 4)
+    lights1 = scan(lights0, algorithm, rowrange, colrange)
+    lights2 = scan(lights1, algorithm, rowrange, colrange)
+    final = set(filter(lambda x: x[0] in range(-2,102) and x[1] in range(-2,102), lights2))
+    print_image(final, -10, 112, -10, 112)
+    return len(final)
+
+def parttwo(lights0, algorithm):
+    n = 50
+    rownumbers = [p[0] for p in lights0]
+    colnumbers = [p[1] for p in lights0]
+    rmin, rmax = min(rownumbers), max(rownumbers)
+    cmin, cmax = min(colnumbers), max(colnumbers)
+    rowrange = range(rmin - 2*n - 5, rmax + 2*n + 6)
+    colrange = range(cmin - 2*n - 5, cmax + 2*n + 6)
+    for k in range(n):
+        lights = scan(lights0, algorithm, rowrange, colrange)
+        lights0 = lights
+    print_image(lights, -60, 160, -60, 160)
+    final = set(filter(lambda x: x[0] in range(cmin-n,cmax+1+n) and x[1] in range(cmin-n,cmax+1+n), lights))
+    print_image(final, -60, 160, -60, 160)
+    return len(final)
 
 main('input.txt')
